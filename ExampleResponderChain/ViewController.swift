e34f262df0a61d805d123a17339b1a0bb48a025b
Valentin <pamerra@mail.ru>
2025-08-13 23:04:17 +0300

    Initial Commit

---
+++ ExampleResponderChain/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExampleResponderChain
+//
+//  Created by Валентин on 13.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
